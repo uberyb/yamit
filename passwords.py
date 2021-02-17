bf12e687d9164f8c787da4d2ad9b444844ee8d07
Dan Tobias <dan.yb.tobias@gmail.com>
2021-02-17 11:01:07 -0800

    separated out settings, added password resets

---
+++ passwords.py
@@ -0,0 +1,65 @@
+from settings import org, api_key, N, csv_file, group_id, notify, speed, pw_mode, activate, reset_time_in_seconds, headers
+import httpx
+import trio
+import json
+
+
+
+
+
+async def get_users_in_group(send_group_users):
+    async with send_group_users:
+        with httpx.Client() as client:
+            rel = f"/api/v1/groups/{group_id}/users?limit=200"
+            r = client.get(org+rel, headers=headers)
+
+
+            while r.json() != []:
+                data = r.json()
+                while r.status_code == 429:
+                    sleep(int(r.headers['x-rate-limit-reset'] - int(time())) + 5)
+                    r = client.get(org+rel, headers=headers)
+                for login in data:
+                    await send_group_users.send(login['id'])
+                try:
+                    next_link = r.headers['link'].split(",")[1].split(";")[0].replace(" ", "").replace("<", "").replace(">","")
+                    r = client.get(next_link,headers=headers)
+                except: break
+            client.close()
+
+            
+
+async def reset_user_password(args):
+    users = args[0]
+    async with users:
+        async for user in users:
+            print(f"Working on {user}")
+            async with httpx.AsyncClient(timeout=120) as client:
+                rel = f"/api/v1/users/{user}/lifecycle/reset_password?sendEmail=true"
+                r = await client.post(org+rel, headers=headers, data=json.dumps({}))
+
+                if r.status_code == 429:
+                        if reset_time_in_seconds != 0:
+                            await trio.sleep(reset_time_in_seconds)
+                            r = await client.post(org+rel, headers=headers, data = json.dumps({}))
+
+                        else:
+                            await trio.sleep(int(r.headers['x-rate-limit-reset']) - int(time()) + 10)
+                            r = await client.post(org+rel, headers=headers, data = json.dumps({}))
+
+                elif r.status_code == 200:
+                    if speed != 100:
+                        limit = int(r.headers['x-rate-limit-limit'])
+                        remaining = int(r.headers['x-rate-limit-remaining'])
+                        if remaining <= (limit * speed/100):
+                            await trio.sleep(int(r.headers['x-rate-limit-reset']))
+
+
+async def main():
+    async with trio.open_nursery() as nursery:
+        send_group_users, recv_channel = trio.open_memory_channel(N)
+        nursery.start_soon(get_users_in_group,send_group_users)
+        for i in range(0,N):
+            nursery.start_soon(reset_user_password, [recv_channel.clone()])
+
+trio.run(main)
---
+++ settings.py
@@ -0,0 +1,27 @@
+import configparser
+
+org, api_key, N, csv_file, group_id, notify, speed, pw_mode, activate, reset_time_in_seconds = ("" for i in range(10))
+attributes = list()
+schema_by_type = dict()
+password_options =  list()
+num_users = 0
+
+print("Loading config...")
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+if len(config.sections()) == 0:
+    print("Config file not found. Please make sure there is a config.ini file in the same directory as app.py.")
+    quit()
+else:
+    org = config['APP_SETTINGS']['ORG']
+    N = int(config['APP_SETTINGS']['MAX_CONCURRENT_SESSIONS'])
+    api_key = config['APP_SETTINGS']['API_TOKEN']
+    csv_file = config['APP_SETTINGS']['CSV_FILE']
+    group_id = config['APP_SETTINGS']['GROUP_ID']
+    notify = int(config['APP_SETTINGS']['NOTIFY'])
+    speed = int(config['APP_SETTINGS']['SPEED'])
+    pw_mode = config['PASSWORD_SETTINGS']['PASSWORD_TYPE']
+    activate = config['APP_SETTINGS']['ACTIVATE'].lower()
+    reset_time_in_seconds = int(config['ADVANCED']['RESET_TIME_IN_SECONDS'])
+    headers = {'Accept': 'application/json', 'Content-Type':'application/json', 'Authorization': f'SSWS {api_key}'}
