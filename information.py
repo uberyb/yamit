9ef08436d60354fd2c12adfb2f8374285bcc468e
Daniel Tobias <dan.yb.tobias@gmail.com>
2021-03-22 22:55:05 -0700

    updated error handling

--- information.py
+++ information.py
@@ -3,6 +3,8 @@ import httpx
 from halo import Halo
 import csv
 
+spinner = Halo(text=f'Loading users CSV...', spinner='dots')
+
 def csv_count(csv_file):
     num = 0
     try:
@@ -18,16 +20,22 @@ def csv_count(csv_file):
 
 def rate_limit(headers,org):
     r = httpx.post(org + '/api/v1/users/me', headers=headers)
+    if r.headers.get('x-rate-limit-limit') == None:
+        spinner.info("API key resulted in an error. Check the ORG url or the API_TOKEN provided in config.ini.")
+        spinner.start()
+
     return r.headers.get('x-rate-limit-limit')
 
 def information():
-    spinner = Halo(text=f'Loading users CSV...', spinner='dots')
-    count = csv_count(csv_file)
-    spinner.succeed("Loaded.")
-    spinner.start()
-    spinner.text = "Getting rate limit for POST to /users"
-    lim = rate_limit(headers,org)
-    spinner.info(f"Rate limit is {lim} per minute.")
-    spinner.start()
-    spinner.info(f"yamit should take {int(count/((speed/100)*int(lim)))} minutes to import {count} users under your current settings.")
+    try:
+        count = csv_count(csv_file)
+        spinner.succeed("Loaded.")
+        spinner.start()
+        spinner.text = "Getting rate limit for POST to /users"
+        lim = rate_limit(headers,org)
+        spinner.info(f"Rate limit is {lim} per minute.")
+        spinner.start()
+        spinner.info(f"yamit should take {int(count/((speed/100)*int(lim)))} minutes to import {count} users under your current settings.")
+    except Exception as e:
+        spinner.info(f"Exception {e}. Check the yamit configuration.")
 
